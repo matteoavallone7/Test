c17cd8c3e8dfb4d32ca6d1eda4dfef75f1e5479d
matteoavallone <matteo.avallone2000@gmail.com>
2026-04-20 12:11:48 +0200

    Test added lyrics

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -7,6 +7,6 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
 	System.out.println("A little context if you care to listen");
-
+	System.out.println("I found myself in a sh*t position");
     }
 }
