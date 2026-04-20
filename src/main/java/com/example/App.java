0871f6d2e4aff23ebf344ba131c0cd7fdb302805
matteoavallone <matteo.avallone2000@gmail.com>
2026-04-20 12:06:32 +0200

    Test trigger automatico

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -6,5 +6,7 @@ package com.example;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+	System.out.println("A little context if you care to listen");
+
     }
 }
